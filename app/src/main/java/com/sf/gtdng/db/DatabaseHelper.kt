1d4ab1175426f22ee45a4d219ce4b788635fe222
Syahrul Fahmi <fahmi@youthmanual.com>
2020-06-09 21:27:45 +0700

    integrate to sqlite

---
+++ app/src/main/java/com/sf/gtdng/db/DatabaseHelper.kt
@@ -0,0 +1,6 @@
+package com.sf.gtdng.db
+
+/**
+ * بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+ * Created By Fahmi on 09/06/20
+ */
---
+++ app/src/main/java/com/sf/gtdng/entity/GithubUserField.kt
@@ -0,0 +1,6 @@
+package com.sf.gtdng.entity
+
+/**
+ * بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+ * Created By Fahmi on 09/06/20
+ */
